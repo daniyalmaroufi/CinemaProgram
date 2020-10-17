1f33efc17886606459e410e9c592c0d5c3eea6d8
Daniyal Marofi <daniyaleight8@gmail.com>
2020-10-17 23:41:39 +0330

    added read data and added print sorted movies list

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <fstream>
-#include <sstream>
 
 using namespace std;
 
-const string SCHEDULE_FILE_NAME = "schedule.csv";
+const string SCHEDULE_FILE_NAME = "./schedule.csv";
 
 struct Movie {
     string CinemaName;
@@ -18,33 +19,68 @@ struct Movie {
     int Hall;
 };
 
-vector<string> ReadHeader(string Line){
+vector<string> ReadHeader(string Line) {
     vector<string> Header;
     string ColName;
     stringstream SStream(Line);
-    while(getline(SStream,ColName,',')){
+    while (getline(SStream, ColName, ',')) {
         Header.push_back(ColName);
     }
     return Header;
 }
 
-void ReadCSVFile() {
-    vector<Movie> Movies;
-    ifstream File;
-    File.open(SCHEDULE_FILE_NAME);
-    string Line;
-    getline(File,Line);
-    vector<string> Header=ReadHeader(Line);
-
-    for(auto ext:Header){
-        cout<<ext<<" ";
+Movie ReadMovie(vector<string> Header, string Line) {
+    string RecordValue;
+    Movie TempMovie;
+    stringstream SStream(Line);
+    for (auto ColName : Header) {
+        getline(SStream, RecordValue, ',');
+        if (ColName == "CinemaName") TempMovie.CinemaName = RecordValue;
+        if (ColName == "MovieName") TempMovie.MovieName = RecordValue;
+        if (ColName == "Day") TempMovie.Day = RecordValue;
+        if (ColName == "StartingTime") TempMovie.StartingTime = RecordValue;
+        if (ColName == "FinishingTime") TempMovie.FinishingTime = RecordValue;
+        if (ColName == "Price") TempMovie.Price = stoi(RecordValue);
+        if (ColName == "Hall") TempMovie.Hall = stoi(RecordValue);
     }
-
-    File.close();
+    return TempMovie;
 }
 
-int main() {
-    ReadCSVFile();
+vector<Movie> ReadCSVFile(string FileName) {
+    vector<Movie> Movies;
+    ifstream File;
+    File.open(FileName);
+    string Line;
+    getline(File, Line);
+    vector<string> Header = ReadHeader(Line);
+    while (getline(File, Line)) {
+        Movies.push_back(ReadMovie(Header, Line));
+    }
+    File.close();
+    return Movies;
+}
+
+vector<string> GetMovieNames(const vector<Movie>& Movies) {
+    vector<string> MovieNames;
+    for (auto Movie : Movies) MovieNames.push_back(Movie.MovieName);
+    return MovieNames;
+}
+
+void PrintMovies(const vector<Movie>& Movies) {
+    vector<string> MovieNames = GetMovieNames(Movies);
+    sort(MovieNames.begin(), MovieNames.end());
+    // remove duplicated values
+    MovieNames.erase(unique(MovieNames.begin(), MovieNames.end()),
+    MovieNames.end());
+    for (auto MovieName : MovieNames) cout << MovieName << endl;
+}
+
+int main(int argc, char* argv[]) {
+    string FileName = SCHEDULE_FILE_NAME;
+    if (argc > 1) FileName = argv[1];
+
+    vector<Movie> Movies = ReadCSVFile(FileName);
+    PrintMovies(Movies);
 
     return 0;
 }
